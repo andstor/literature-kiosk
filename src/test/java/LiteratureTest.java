03ab96c0be009a85d8c0bea48cc1afd595f5464b
André Storhaug <andr3.storhaug@gmail.com>
2018-04-11 16:10:20 +0200

    Create JUnit test LiteratureTest

---
+++ src/test/java/LiteratureTest.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LiteratureTest {
+    private Literature literature;
+
+    @BeforeEach
+    void setUp() {
+        literature = new Magazine("title", "publisher", 100, "genre");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    /*@Test
+    void getType() {
+        assertEquals("type", literature.getType());
+    }*/
+
+
+    /*@Test
+    void setType() {
+        literature.setType("new type");
+        assertEquals("new type", literature.getType());
+    }*/
+
+    @Test
+    void getTitle() {
+        assertEquals("title", literature.getTitle());
+    }
+
+
+    @Test
+    void setTitle() {
+        literature.setTitle("new title");
+        assertEquals("new title", literature.getTitle());
+    }
+
+    @Test
+    void getPublisher() {
+        assertEquals("publisher", literature.getPublisher());
+    }
+
+    @Test
+    void setPublisher() {
+        literature.setPublisher("new publisher");
+        assertEquals("new publisher", literature.getPublisher());
+    }
+
+    @Test
+    void getGenre() {
+        assertEquals("genre", literature.getGenre());
+    }
+
+
+    @Test
+    void setGenre() {
+        literature.setGenre("new genre");
+        assertEquals("new genre", literature.getGenre());
+    }
+}
