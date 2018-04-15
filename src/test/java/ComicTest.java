67663a0c5595be1a957167fa349a9c631c8e73e9
André Storhaug <andr3.storhaug@gmail.com>
2018-04-15 22:04:45 +0200

    Create comic test

---
+++ src/test/java/ComicTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ComicTest {
+    private Comic comic;
+
+    @BeforeEach
+    void setUp() {
+        comic = new Comic("title", "publisher", 100, "genre");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPublicationsYearly() {
+        assertEquals(100, comic.getPublicationsYearly());
+    }
+
+    @Test
+    void getType() {
+        assertEquals("comic", comic.getType());
+    }
+}
